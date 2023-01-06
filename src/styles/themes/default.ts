26e9178194ebc7577540860cdab16710b617fd72
Gustavo Albunio <gustav.albunio@gmail.com>
2023-01-06 17:36:00 -0300

    Primeiro commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#ffffff',
+
+  'gray-100': '#fafafa',
+  'gray-200': '#f3f2f2',
+  'gray-300': '#ededed',
+  'gray-400': '#e6e5e5',
+  'gray-500': '#d7d5d5',
+  'gray-600': '#8d8686',
+  'gray-700': '#574f4d',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  'purply-100': '#ebe5f9',
+  'purply-300': '#8047f8',
+  'purply-500': '#4b2995',
+
+  'yellow-100': '#f1e9c9',
+  'yellow-300': '#dbac2c',
+  'yellow-500': '#c47f17',
+}
